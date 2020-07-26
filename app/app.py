9f476304fe89caaf84e0423908865d4312969193
Mike Rock <mr392@njit.edu>
2020-07-26 19:31:48 -0400

    Fix: db

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, Response, redirect
 from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
-import app.Calculator as calc
+
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
@@ -13,142 +13,17 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'numberData'
+app.config['MYSQL_DATABASE_DB'] = 'peopleData'
 mysql.init_app(app)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    user = {'username': 'Mike'}
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblPeopleImport order by person_num ASC')
-    result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, people=result)
 
-
-@app.route('/view/<int:person_num>', methods=['GET'])
-def record_view(person_num):
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblPeopleImport WHERE person_num=%s', person_num)
-    result = cursor.fetchall()
-    return render_template('view.html', title='View Form', person=result[0])
-
-
-@app.route('/edit/<int:person_num>', methods=['GET'])
-def form_edit_get(person_num):
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblPeopleImport WHERE person_num=%s', person_num)
-    result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', person=result[0])
-
-
-@app.route('/edit/<int:person_num>', methods=['POST'])
-def form_update_post(person_num):
-    cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('person_num'), request.form.get('height'), request.form.get('weight'),
-                person_num)
-    sql_update_query = """UPDATE tblPeopleImport t SET t.person_num = %s, t.height = %s, t.weight = %s  WHERE t.person_num = %s """
-    cursor.execute(sql_update_query, inputData)
-    mysql.get_db().commit()
-    return redirect("/", code=302)
-
-@app.route('/person/new', methods=['GET'])
-def form_insert_get():
-    return render_template('new.html', title='New City Form')
-
-
-@app.route('/person/new', methods=['POST'])
-def form_insert_post():
-    cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('person_num'), request.form.get('height'), request.form.get('weight'))
-    sql_insert_query = """INSERT INTO tblPeopleImport (person_num, height, weight) VALUES (%s, %s,%s) """
-    cursor.execute(sql_insert_query, inputData)
-    mysql.get_db().commit()
-    return redirect("/", code=302)
-
-@app.route('/delete/<int:person_num>', methods=['POST'])
-def form_delete_post(person_num):
-    cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM tblPeopleImport WHERE person_num = %s """
-    cursor.execute(sql_delete_query, person_num)
-    mysql.get_db().commit()
-    return redirect("/", code=302)
-
-# --------------------------------------------
-
-@app.route('/api/persons', methods=['GET'])
-def api_browse() -> str:
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblPeopleImport')
-    result = cursor.fetchall()
-    json_result = json.dumps(result);
-    resp = Response(json_result, status=200, mimetype='application/json')
-    return resp
-
-
-@app.route('/api/persons/<int:person_num>', methods=['GET'])
-def api_retrieve(person_num) -> str:
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblPeopleImport WHERE person_num=%s', person_num)
-    result = cursor.fetchall()
-    json_result = json.dumps(result);
-    resp = Response(json_result, status=200, mimetype='application/json')
-    return resp
-
-
-@app.route('/api/persons', methods=['POST'])
-def api_add() -> str:
-    cursor = mysql.get_db().cursor()
-    content = request.json
-    inputData = (content['person_num'], content['Height'], content['Weight'])
-    sql_add_query = """INSERT INTO tblPeopleImport (person_num, Height, Weight) VALUES (%s, %s, %s) """
-    cursor.execute(sql_add_query, inputData)
-    mysql.get_db().commit()
-    resp = Response(status=201, mimetype='application/json')
-    return resp
-
-
-
-@app.route('/api/persons/<int:person_num>', methods=['PUT'])
-def api_edit(person_num) -> str:
-    cursor = mysql.get_db().cursor()
-    content = request.json
-    inputData = (content['person_num'], content['Height'], content['Weight'], person_num)
-    sql_update_query = """UPDATE tblPeopleImport p SET p.person_num = %s, p.Height = %s, p.weight = %s WHERE p.person_num = %s """
-    cursor.execute(sql_update_query, inputData)
-    mysql.get_db().commit()
-    resp = Response(status=200, mimetype='application/json')
-    return resp
-
-
-@app.route('/api/persons/<int:person_num>', methods=['DELETE'])
-def api_delete(person_num) -> str:
-    cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM tblPeopleImport WHERE person_num = %s """
-    cursor.execute(sql_delete_query, person_num)
-    mysql.get_db().commit()
-    resp = Response(status=210, mimetype='application/json')
-    return resp
-# ----------------------------------------------
-@app.route('/api/add_num', methods=['POST'])
-def api_add_num() -> str:
-    cursor = mysql.get_db().cursor()
-    content = request.json
-    result = calc.addition(content)
-
-
-    inputData = (content['num1'], content['num2'], content['result'])
-    sql_add_query = """INSERT INTO numberImport (num1, num2, result) VALUES (%s, %s, %s) """
-    cursor.execute(sql_add_query, inputData)
-    mysql.get_db().commit()
-    resp = Response(status=201, mimetype='application/json')
-    return resp
 
 
 @app.route('/api/numbers', methods=['GET'])
-def api_browse() -> str:
+def api_num_browse() -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM numberImport')
+    cursor.execute('SELECT * FROM tblPeopleImport')
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
--- db/init.sql
+++ db/init.sql
@@ -2,17 +2,13 @@ CREATE DATABASE peopleData;
 use peopleData;
 
 CREATE TABLE IF NOT EXISTS tblPeopleImport (
-    `index` INT,
-    `num1` NUMERIC(5,2),
-    `num2` NUMERIC(5, 2),
+
+    `person_num` INT,
+    `Height` NUMERIC(5, 2),
+    `Weight` NUMERIC(5, 2),
     `result` NUMERIC(5, 2),
 
-    PRIMARY KEY (`index`)
+    PRIMARY KEY (`person_num`)
 );
-INSERT INTO tblPeopleImport (index, num1, num2, result) VALUES
-    (1, 1, 65.78, 112.99),
-    (2, 71.52, 136.49),
-    (3, 69.40, 153.03),
-    (4, 67.76, 114.56),
-    (4, 68.02, 123.49),
-    (6, 67.66, 123.05);
+INSERT INTO tblPeopleImport (person_num, height, weight, result) VALUES
+    (1, 65.78, 5, 112.99);
