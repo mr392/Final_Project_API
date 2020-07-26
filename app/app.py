840aaaa763c8c966d4f84f5baaea83946c26a7d3
Mike Rock <mr392@njit.edu>
2020-07-26 19:05:32 -0400

    Task: setup new db

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, Response, redirect
 from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+import app.Calculator as calc
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
@@ -12,7 +13,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'peopleData'
+app.config['MYSQL_DATABASE_DB'] = 'numberData'
 mysql.init_app(app)
 
 
@@ -128,6 +129,31 @@ def api_delete(person_num) -> str:
     mysql.get_db().commit()
     resp = Response(status=210, mimetype='application/json')
     return resp
+# ----------------------------------------------
+@app.route('/api/add_num', methods=['POST'])
+def api_add_num() -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    result = calc.addition(content)
+
+
+    inputData = (content['num1'], content['num2'], content['result'])
+    sql_add_query = """INSERT INTO numberImport (num1, num2, result) VALUES (%s, %s, %s) """
+    cursor.execute(sql_add_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/numbers', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM numberImport')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
 
 
 if __name__ == '__main__':
