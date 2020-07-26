bc4cea6ece5b3ac4109781cb8f34d8834190311f
Mike Rock <mr392@njit.edu>
2020-07-26 19:56:42 -0400

    Fix: db

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'peopleData'
+app.config['MYSQL_DATABASE_DB'] = 'numberData'
 mysql.init_app(app)
 
 
--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,12 @@
-CREATE DATABASE peopleData;
-use peopleData;
+CREATE DATABASE numberData;
+use numberData;
 
 CREATE TABLE IF NOT EXISTS tblPeopleImport (
 
-    `person_num` INT,
-    `Height` NUMERIC(5, 2),
-    `Weight` NUMERIC(5, 2),
-    `result` NUMERIC(5, 2),
 
-    PRIMARY KEY (`person_num`)
-);
-INSERT INTO tblPeopleImport (person_num, height, weight, result) VALUES
-    (1, 65.78, 5, 112.99);
+    `num1` NUMERIC(5, 2),
+    `num2` NUMERIC(5, 2),
+    `result` NUMERIC(5, 2)
+    );
+INSERT INTO tblPeopleImport (num1, num2, result) VALUES
+    (65.78, 5, 112.99);
