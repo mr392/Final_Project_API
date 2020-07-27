e5ca23dbdfdd7bdc8d354f6b25dcef750fbb5c31
Mike Rock <mr392@njit.edu>
2020-07-26 20:05:38 -0400

    Fix: #2 DB

--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE numberData;
 use numberData;
 
-CREATE TABLE IF NOT EXISTS tblPeopleImport (
+CREATE TABLE IF NOT EXISTS numberImport (
 
 
     `num1` NUMERIC(5, 2),
     `num2` NUMERIC(5, 2),
     `result` NUMERIC(5, 2)
     );
-INSERT INTO tblPeopleImport (num1, num2, result) VALUES
+INSERT INTO numberImport (num1, num2, result) VALUES
     (65.78, 5, 112.99);
