80b00acce8baef6be71a0904a6448366cb4bf9be
Mike Rock <mr392@njit.edu>
2020-07-27 20:07:53 -0400

    Fix: #5 DB

--- db/init.sql
+++ db/init.sql
@@ -3,11 +3,10 @@ use numberData;
 
 CREATE TABLE IF NOT EXISTS numberImport (
 
-
+    `id` INT,
     `num1` NUMERIC(5, 2),
     `num2` NUMERIC(5, 2),
-    `result` NUMERIC(5, 2),
-    `p` NUMERIC(5, 2)
-    );
-INSERT INTO numberImport (num1, num2, result, p) VALUES
-    (65.78, 5, 112.99, 1);
+    `result` NUMERIC(5, 2)
+   );
+INSERT INTO numberImport (id, num1, num2, result) VALUES
+    (1, 65.78, 5, 112.99);
