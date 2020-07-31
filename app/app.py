e336be7cee8a29d9e3baec75e33be4d5108fed69
Mike Rock <mr392@njit.edu>
2020-07-31 15:54:47 -0400

    Task: create chart html

--- app/app.py
+++ app/app.py
@@ -17,13 +17,36 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'numberData'
 mysql.init_app(app)
 
+
+
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+
 @app.route('/', methods=['GET'])
 def index():
+    bar_values = values
+    bar_labels = labels
     user = {'username': 'Mike'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM numberImport')
     result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, num_result=result)
+    return render_template('index.html', title='Home', user=user, num_result=result, labels=bar_labels, values=bar_values)
 
 
 
@@ -82,6 +105,15 @@ def api_delete(id) -> str:
     resp = Response(status=210, mimetype='application/json')
     return resp
 
+@app.route('/bar',  methods=['GET'])
+def bar():
+    bar_labels=labels
+    bar_values=values
+    user = {'username': 'Mike'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM numberImport')
+    result = cursor.fetchall()
+    return render_template('bar_chart.html', title='Bitcoin Monthly Price in USD', max=17000, num_result=result, labels=bar_labels, values=bar_values)
 
 
 
