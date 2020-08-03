cea5f9e08cb361fc8332da58b3266e87ff4b067b
Mike Rock <mr392@njit.edu>
2020-08-03 16:11:40 -0400

    Fix: db

--- db/init.sql
+++ db/init.sql
@@ -11,9 +11,12 @@ CREATE TABLE IF NOT EXISTS numberImport (
     PRIMARY KEY (`id`)
    );
 INSERT INTO numberImport (id, num1, num2, operation, result) VALUES
-    (1, 65.78, 5, "add",  70.78);
-
-
+    (1, 65.78, 5, "add",  70.78),
+    (2, 25, 4, "multiply", 100),
+    (3, 10, 9, "subtract", 1),
+    (4, 100, 10, "divide", 10),
+    (5, 5, 0, "sqaure", 25),
+    (6, 100, 0, "sqrt", 10);
 
 
 CREATE TABLE IF NOT EXISTS statsImport (
@@ -29,5 +32,6 @@ CREATE TABLE IF NOT EXISTS statsImport (
     `result` NUMERIC(5, 2),
     PRIMARY KEY (`id`)
    );
-INSERT INTO statsImport (num1, num2,num3,num4,num5,num6, operation, result) VALUES
-    (1, 65.78, 5, 2.2, 9.0, 6.7,  "mean",  70.78);
+INSERT INTO statsImport (id, num1, num2,num3,num4,num5,num6, operation, result) VALUES
+    (1, 1, 65.78, 5, 2.2, 9.0, 6.7, "mean",  70.78),
+    (2, 2, 3, 4, 5, 6, 7, "mean", 4);
